6f301763232d97a5ec62ed19e38dbf46f2662372
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-10-08 19:17:11 +0200

    Create marketReducer.js

---
+++ src/reducers/marketReducer.js
@@ -0,0 +1,22 @@
+// src/reducers/marketReducer.js
+import { SET_MARKET_PAIR } from '../actions/types';
+
+const initialState = {
+  marketPair: 'DIST/NXS',
+  // other state properties
+};
+
+const marketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_MARKET_PAIR:
+      return {
+        ...state,
+        marketPair: action.payload,
+      };
+    // other cases
+    default:
+      return state;
+  }
+};
+
+export default marketReducer;
