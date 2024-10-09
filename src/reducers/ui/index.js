c9779944134d379c7deaffad573d7a499db0cce7
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-10-09 10:54:58 +0200

    Updates

--- src/reducers/ui/index.js
+++ src/reducers/ui/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import inputValueReducer from './inputValue';
+import marketReducer from './marketReducer';
 
 export default combineReducers({
   ui: inputValueReducer,
+  market: marketReducer,
 });
