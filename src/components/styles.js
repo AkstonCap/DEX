2ec21065089a291ebcbe936187317fb78df75e1a
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-18 21:50:33 +0100

    Create styles.js

---
+++ src/components/styles.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const overviewGridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr; /* Left column takes 1 part, right column takes 2 parts */
+  gap: 20px; /* Space between columns */
+`;
