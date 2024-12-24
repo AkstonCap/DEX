19e86968dad05c61509ef63945d230bf46cc7415
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-24 23:26:57 +0100

    Update PersonalTradeHistory.js

--- src/components/PersonalTradeHistory.js
+++ src/components/PersonalTradeHistory.js
@@ -1,34 +1,24 @@
 import { FieldSet } from 'nexus-module';
+//import { useSelector } from 'react-redux';
+import { renderExecutedOrders } from './TradeHistory';
 
 export default function PersonalTradeHistory() {
+    //const executedOrders = useSelector((state) => state.ui.market.executedData.executedOrders);
+
     return (
         <div className="mt2">
-            <FieldSet legend="My Executed Orders">
+            <FieldSet legend="My Transactions">
                 <div>
                     <table>
                         <thead>
                             <tr>
-                                <th>Price</th>
-                                <th>Amount</th>
+                                <th>Price [{baseToken}/{orderToken}]</th>
+                                <th>Amount {orderToken}</th>
                                 <th>Time</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>0.0000001 BTC</td>
-                                <td>1000 NXS</td>
-                                <td>2021-09-01 12:00:00</td>
-                            </tr>
-                            <tr>
-                                <td>0.0000002 BTC</td>
-                                <td>500 NXS</td>
-                                <td>2021-09-01 12:05:00</td>
-                            </tr>
-                            <tr>
-                                <td>0.0000003 BTC</td>
-                                <td>200 NXS</td>
-                                <td>2021-09-01 12:10:00</td>
-                            </tr>
+                            {renderExecutedOrders()}
                         </tbody>
                     </table>
                 </div>
