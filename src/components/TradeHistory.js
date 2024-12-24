1719289663335af96c04c5e4424cbdca7e1d4651
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-24 23:27:01 +0100

    Update TradeHistory.js

--- src/components/TradeHistory.js
+++ src/components/TradeHistory.js
@@ -1,45 +1,45 @@
 import { useSelector } from 'react-redux';
 import { FieldSet } from 'nexus-module';
 
-export default function TradeHistory() {
+export const renderExecutedOrders = () => {
   const executedOrders = useSelector((state) => state.ui.market.executedData.executedOrders);
+  const bids = executedOrders && Array.isArray(executedOrders.bids) ? executedOrders.bids : [];
+  const asks = executedOrders && Array.isArray(executedOrders.asks) ? executedOrders.asks : [];
 
-  const renderExecutedOrders = () => {
-    const bids = executedOrders && Array.isArray(executedOrders.bids) ? executedOrders.bids : [];
-    const asks = executedOrders && Array.isArray(executedOrders.asks) ? executedOrders.asks : [];
-
-    if (bids.length === 0 && asks.length === 0) {
-      return (
-        <tr>
-          <td colSpan="3">No executed orders</td>
-        </tr>
-      );
-    }
-    asks.forEach((element) => {
-      element.order.amount = element.contract.amount;
-    });
-    const sortedExecutedOrders = [...bids, ...asks].sort(
-      (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
-    );
-
-    return sortedExecutedOrders.map((order, index) => (
-      <tr key={index}>
-        <td>{`${order.price} ${baseToken}`}</td>
-        <td>{`${order.order.amount} ${orderToken}`}</td>
-        <td>{new Date(order.timestamp).toLocaleString()}</td>
+  if (bids.length === 0 && asks.length === 0) {
+    return (
+      <tr>
+        <td colSpan="3">No executed orders</td>
       </tr>
-    ));
-  };
+    );
+  }
+  asks.forEach((element) => {
+    element.order.amount = element.contract.amount;
+  });
+  const sortedExecutedOrders = [...bids, ...asks].sort(
+    (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+  );
+
+  return sortedExecutedOrders.map((order, index) => (
+    <tr key={index}>
+      <td>{`${order.price} ${baseToken}`}</td>
+      <td>{`${order.order.amount} ${orderToken}`}</td>
+      <td>{new Date(order.timestamp).toLocaleString()}</td>
+    </tr>
+  ));
+};
+
+export default function TradeHistory() {
 
   return (
     <div className="mt2">
-      <FieldSet legend="Executed Orders">
+      <FieldSet legend="Transactions">
           <div>
             <table>
               <thead>
                 <tr>
-                  <th>Price</th>
-                  <th>Amount</th>
+                  <th>Price [{baseToken}/{orderToken}]</th>
+                  <th>Amount {orderToken}</th>
                   <th>Time</th>
                 </tr>
               </thead>
