67f1347769fc94a9e80406701b69dc6e42158a2f
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-25 20:12:49 +0100

    Update OrderBookComp.js

--- src/components/OrderBookComp.js
+++ src/components/OrderBookComp.js
@@ -1,20 +1,31 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { FieldSet } from 'nexus-module';
+import { setOrder } from 'actions/actionCreators';
+import { gridStyleOrderbook } from './styles';
 
 export default function OrderBookComp() {
+  const dispatch = useDispatch();
   const orderBook = useSelector((state) => state.ui.market.orderBook);
-  const orderToken = useSelector((state) => state.ui.market.marketPairs.orderToken);
+  const quoteToken = useSelector((state) => state.ui.market.marketPairs.quoteToken);
   const baseToken = useSelector((state) => state.ui.market.marketPairs.baseToken);
+
+  const handleOrderClick = (order) => {
+    if (order.type === 'ask') {
+      dispatch(setOrder(order.address, order.price, order.order.amount));
+    } else if (order.type === 'bid') {
+      dispatch(setOrder(order.address, order.price, order.contract.amount));
+    }
+  };
 
   const renderTableBids = (data) => {
     if (!Array.isArray(data)) {
       return null;
     }
     return data.slice(0, 5).map((item, index) => (
-      <tr key={index}>
+      <tr key={index}> onClick={() => handleOrderClick(item)}
         <td>{item.price}</td>
-        <td>{`${item.order.amount} ${orderToken}`}</td>
-        <td>{`${item.contract.amount} ${baseToken}`}</td>
+        <td>{`${item.order.amount} ${baseToken}`}</td>
+        <td>{`${item.contract.amount} ${quoteToken}`}</td>
       </tr>
     ));
   };
@@ -24,10 +35,10 @@ export default function OrderBookComp() {
       return null;
     }
     return data.slice(0, 5).map((item, index) => (
-      <tr key={index}>
+      <tr key={index}> onClick={() => handleOrderClick(item)}
         <td>{item.price}</td>
-        <td>{`${item.contract.amount} ${orderToken}`}</td>
-        <td>{`${item.order.amount} ${baseToken}`}</td>
+        <td>{`${item.contract.amount} ${baseToken}`}</td>
+        <td>{`${item.order.amount} ${quoteToken}`}</td>
       </tr>
     ));
   };
@@ -43,8 +54,8 @@ export default function OrderBookComp() {
                 <thead>
                   <tr>
                     <th>Price</th>
-                    <th>Amount {orderToken}</th>
                     <th>Amount {baseToken}</th>
+                    <th>Amount {quoteToken}</th>
                   </tr>
                 </thead>
                 <tbody>{renderTableAsks(orderBook.asks)}</tbody>
@@ -56,8 +67,8 @@ export default function OrderBookComp() {
                 <thead>
                   <tr>
                     <th>Price</th>
-                    <th>Amount {orderToken}</th>
                     <th>Amount {baseToken}</th>
+                    <th>Amount {quoteToken}</th>
                   </tr>
                 </thead>
                 <tbody>{renderTableBids(orderBook.bids)}</tbody>
