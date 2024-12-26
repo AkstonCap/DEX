0802cc391e1e90b0093d5d5049203792b958dd8a
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-26 14:19:46 +0100

    Update types.js

--- src/actions/types.js
+++ src/actions/types.js
@@ -9,3 +9,7 @@ export const SET_EXECUTED_ORDERS = 'SET_EXECUTED_ORDERS';
 export const SWITCH_TAB = 'SWITCH_TAB';
 
 export const SET_ORDER = 'SET_ORDER';
+
+export const SET_MY_ORDERS = 'SET_MY_ORDERS';
+
+export const SET_MY_TRADES = 'SET_MY_TRADES';
