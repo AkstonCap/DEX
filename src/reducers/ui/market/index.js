1ab2f929b7570549e2bb9aaa50cc155334895823
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-26 14:20:32 +0100

    Update index.js

--- src/reducers/ui/market/index.js
+++ src/reducers/ui/market/index.js
@@ -4,10 +4,14 @@ import marketPairs from './marketPairs';
 import orderBook from './orderBook';
 import executedData from './executedData';
 import orderInQuestion from './orderInQuestion';
+import myTrades from './myTrades';
+import myOrders from './myOrders';
 
 export default combineReducers({
   marketPairs,
   orderBook,
   executedData,
   orderInQuestion,
+  myTrades,
+  myOrders,
 });
