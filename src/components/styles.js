35c1e856c3295b1c825fa794085c0582f6de50af
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-29 17:10:00 +0100

    Update styles.js

--- src/components/styles.js
+++ src/components/styles.js
@@ -11,3 +11,27 @@ export const tradeGridContainer = styled.div`
   grid-template-columns: 2fr 2fr; /* Left column takes 2 parts, right column takes 2 parts */
   gap: 20px; /* Space between columns */
 `;
+
+export const PageLayout = styled.div`
+  display: grid;
+  grid-template-rows: 60% 40%;
+  height: 100vh;
+  gap: 10px;
+`;
+
+export const TopRow = styled.div`
+  display: grid;
+  grid-template-columns: 60% 40%;
+  gap: 10px;
+`;
+
+export const BottomRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px;
+`;
+
+export const TradingContainer = styled.div`
+  background: #f5f5f5;
+  padding: 10px;
+`;
