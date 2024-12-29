bdc272ce354b5a2d8b85153570b2dbdf597814be
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-29 17:09:47 +0100

    Update trade.js

--- src/App/trade.js
+++ src/App/trade.js
@@ -4,8 +4,12 @@ import TradeForm from 'components/TradeForm';
 import TradeHistory from 'components/TradeHistory';
 import PersonalTradeHistory from 'components/PersonalTradeHistory';
 import PersonalOpenOrders from 'components/PersonalOpenOrders';
-import { tradeGridContainer } from 'components/styles';
-import 'components/layout.css';
+import { 
+  PageLayout, 
+  TopRow, 
+  BottomRow, 
+  TradingContainer 
+} from 'components/styles';
 
 export default function Trade() {
   const marketPair = useSelector((state) => state.ui.market.marketPairs.marketPair);
@@ -14,27 +18,27 @@ export default function Trade() {
     <div className="mt2">
       <h1>Trading Dashboard</h1>
       <h2>{marketPair}</h2>
-      <div className="overview-page">
-            <div className='top-row'>
-              <div className='trading-container'>
-                <TradeForm />
-              </div>
-              <div className='orderbook-container'>
-                <OrderBookComp />
-              </div>
-            </div>
-            <div className='bottom-row'>
-              <div trade-history-container>
-                <TradeHistory />
-              </div>
-              <div className="personal-trade-history-container">
-                <PersonalTradeHistory />
-              </div>
-              <div className="personal-open-orders-container">
-                <PersonalOpenOrders />
-              </div>
-            </div>
-          </div>
+      <PageLayout>
+        <TopRow>
+          <TradingContainer>
+            <TradeForm />
+          </TradingContainer>
+          <TradingContainer>
+            <OrderBookComp />
+          </TradingContainer>
+        </TopRow>
+        <BottomRow>
+          <TradingContainer>
+            <TradeHistory />
+          </TradingContainer>
+          <TradingContainer>
+            <PersonalTradeHistory />
+          </TradingContainer>
+          <TradingContainer>
+            <PersonalOpenOrders />
+          </TradingContainer>
+        </BottomRow>
+      </PageLayout>
     </div>
   );
 }
