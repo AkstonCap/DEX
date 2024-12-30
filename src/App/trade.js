b295101a1e31b7c56443ffad9891e2189652af2a
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-30 14:27:40 +0100

    Update trade.js

--- src/App/trade.js
+++ src/App/trade.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+//import { useSelector } from 'react-redux';
 import OrderBookComp from 'components/OrderBookComp';
 import TradeForm from 'components/TradeForm';
 import TradeHistory from 'components/TradeHistory';
@@ -11,23 +11,19 @@ import {
 } from 'components/styles';
 
 export default function Trade() {
-  const marketPair = useSelector((state) => state.ui.market.marketPairs.marketPair);
+  //const marketPair = useSelector((state) => state.ui.market.marketPairs.marketPair);
 
   return (
-    <div className="mt2">
-      <h1>Trading Dashboard</h1>
-      <h2>{marketPair}</h2>
-      <PageLayout>
-        <TopRow>
-          <TradeForm />
-          <OrderBookComp />
-        </TopRow>
-        <BottomRow>
-          <TradeHistory />
-          <PersonalTradeHistory />
-          <PersonalOpenOrders />
-        </BottomRow>
-      </PageLayout>
-    </div>
+    <PageLayout>
+      <TopRow>
+        <TradeForm />
+        <OrderBookComp />
+      </TopRow>
+      <BottomRow>
+        <TradeHistory />
+        <PersonalTradeHistory />
+        <PersonalOpenOrders />
+      </BottomRow>
+    </PageLayout>
   );
 }
