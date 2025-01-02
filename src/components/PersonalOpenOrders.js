050ed187cb4928b83d2f177c658f7abb1151ff66
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-02 18:31:37 +0100

    Update PersonalOpenOrders.js

--- src/components/PersonalOpenOrders.js
+++ src/components/PersonalOpenOrders.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { FieldSet } from 'nexus-module';
 import { OrderTable } from './styles';
+import DeleteButton from './DeleteButton';
 
 export default function PersonalOpenOrders() {
   const baseToken = useSelector((state) => state.ui.market.marketPairs.baseToken);
@@ -29,41 +30,45 @@ export default function PersonalOpenOrders() {
         </FieldSet>
       </div>
     );
+  } else {
+
+    // Adjust 'asks' so that order.amount matches contract.amount
+    if (myOrders.asks.length > 0) {
+      myOrders.asks.forEach((element) => {
+        element.order.amount = element.contract.amount;
+      });
+    };
+
+    // Merge and sort
+    const sortedOrders = [...myOrders.bids, ...myOrders.asks].sort(
+      (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+    );
+
+    // Convert each order into a table row
+    const rows = sortedOrders.map((order, index) => (
+      <tr key={index}>
+        <td>{`${order.price} ${quoteToken}`}</td>
+        <td>{`${order.order.amount} ${baseToken}`}</td>
+        <td>{new Date(order.timestamp).toLocaleString()}</td>
+        <DeleteButton order={order.address} />
+      </tr>
+    ));
+
+    return (
+      <div className="mt2">
+        <FieldSet legend="My Open Orders">
+          <OrderTable>
+            <thead>
+              <tr>
+                <th>Price [{quoteToken}/{baseToken}]</th>
+                <th>Amount {baseToken}</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </OrderTable>
+        </FieldSet>
+      </div>
+    );
   }
-
-  // Adjust 'asks' so that order.amount matches contract.amount
-  myOrders.asks.forEach((element) => {
-    element.order.amount = element.contract.amount;
-  });
-
-  // Merge and sort
-  const sortedOrders = [...myOrders.bids, ...myOrders.asks].sort(
-    (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
-  );
-
-  // Convert each order into a table row
-  const rows = sortedOrders.map((order, index) => (
-    <tr key={index}>
-      <td>{`${order.price} ${quoteToken}`}</td>
-      <td>{`${order.order.amount} ${baseToken}`}</td>
-      <td>{new Date(order.timestamp).toLocaleString()}</td>
-    </tr>
-  ));
-
-  return (
-    <div className="mt2">
-      <FieldSet legend="My Open Orders">
-        <table>
-          <thead>
-            <tr>
-              <th>Price [{quoteToken}/{baseToken}]</th>
-              <th>Amount {baseToken}</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      </FieldSet>
-    </div>
-  );
 }
