196f09d2f8b671cf4f02103b51b53d31c0260b09
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-04 13:45:28 +0100

    Update chart.js

--- src/App/chart.js
+++ src/App/chart.js
@@ -1,12 +1,16 @@
+import { ChartPageLayout } from "components/styles";
+import TradeHistory from "components/TradeHistory";
+
 export default function Chart() {
     return (
-      <>
+      <ChartPageLayout>
         <div className="Chart">
           <h1>Welcome to the DEX</h1>
           <p>
             Chart TBA
           </p>
         </div>
-      </>
+        <TradeHistory num={20} />
+      </ChartPageLayout>
     );
 }
