838e2517d721f6cbfcf0663abd2b7e9f1cbf7015
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-04 13:45:41 +0100

    Update overview.js

--- src/App/overview.js
+++ src/App/overview.js
@@ -3,6 +3,9 @@ import { useSelector } from 'react-redux';
 import { 
   FieldSet,
   Arrow,
+  Dropdown,
+  Select,
+  FormField,
  } from 'nexus-module';
 import { fetchVolumeData } from 'actions/fetchVolumeData';
 import { fetchMarketData } from 'actions/fetchMarketData';
@@ -11,6 +14,9 @@ import {
   TopRow,
   PageLayout,
   ChangeText,
+  Line,
+  Value,
+  Label,
  } from 'components/styles';
 import OrderBookComp from 'components/OrderBookComp';
 import TradeHistory from 'components/TradeHistory';
@@ -25,15 +31,46 @@ export default function Overview() {
 
   const orderBook = useSelector((state) => state.ui.market.orderBook);
   const executedOrders = useSelector(
-    (state) => state.ui.market.executedData.executedOrders
+    (state) => state.ui.market.executedOrders
   );
 
   // Declare state variables
   const [baseTokenVolume, setBaseTokenVolume] = useState(0);
   const [quoteTokenVolume, setQuoteTokenVolume] = useState(0);
-  const [lastPrice, setLastPrice] = useState('N/A');
+  const [lastPrice, setLastPrice] = useState(0);
+  const [high, setHigh] = useState(0);
+  const [low, setLow] = useState(0);
+  const [change, setChange] = useState(0);
+  const [timeFrame, setTimeFrame] = useState('1y');
   const [highestBid, setHighestBid] = useState('N/A');
   const [lowestAsk, setLowestAsk] = useState('N/A');
+
+  const timeFrames = [
+    {
+      value: 'all',
+      display: ('All'),
+    },
+    {
+      value: '1y',
+      display: ('1 Year'),
+    },
+    {
+      value: '1m',
+      display: ('1 Month'),
+    },
+    {
+      value: '1w',
+      display: ('1 Week'),
+    },
+    {
+      value: '1d',
+      display: ('1 Day'),
+    },
+    {
+      value: '1h',
+      display: ('1 Hour'),
+    },
+  ];
 
   // Define updateData function at the component level
   const updateData = () => {
@@ -58,7 +95,9 @@ export default function Overview() {
     }
 
     setHighestBid(orderBook?.bids?.[0]?.price || 'N/A');
-    setLowestAsk(orderBook?.asks?.[0]?.price || 'N/A');
+    setLowestAsk(
+      orderBook?.asks?.[orderBook?.asks?.length - 1]?.price || 'N/A'
+    );
   };
 
   useEffect(() => {
@@ -79,25 +118,52 @@ export default function Overview() {
   return (
     <PageLayout>
       <TopRow>
-        <div className='mt2'>
+        <div>
           <FieldSet legend={`${marketPair} overview`}>
-            <p>
-              Last Price: {
-              lastPrice !== null ? `${lastPrice} ${quoteToken}` : 'N/A'
-              } <ChangeText Arrow={Arrow} />
-            </p>
-            <p>
-              Bid/Ask: {highestBid} {quoteToken} / {lowestAsk} {quoteToken}
-            </p>
-            <p>
-              1yr Volume: {baseTokenVolume} {baseToken} / {quoteTokenVolume} {quoteToken}
-            </p>
+            
+            <Line>
+              <div>
+                <Label>Last Price:</Label>
+                <Value>{lastPrice}</Value>
+              </div>
+              <div>
+                <Label>High</Label>
+                <Value>{high}</Value>
+              </div>
+              <div>
+                <Label>Volume ({baseToken})</Label>
+                <Value>{baseTokenVolume}</Value>
+              </div>
+            </Line>
+            <Line>
+              <div>
+                <Label>Change:</Label>
+                <Value>{change} %</Value>
+              </div>
+              <div>
+                <Label>Low</Label>
+                <Value>{low}</Value>
+              </div>
+              <div>
+                <Label>Volume ({quoteToken})</Label>
+                <Value>{quoteTokenVolume}</Value>
+              </div>
+            </Line>
+            <div className='mt3'>
+              <FormField label={('Time span')}>
+                <Select
+                  value={timeFrame}
+                  onChange={(val) => dispatch(setTimeFrame(val))}
+                  options={timeFrames}
+                />
+              </FormField>
+            </div>
           </FieldSet>
         </div>
-        <OrderBookComp />
+        <OrderBookComp num={6} />
       </TopRow>
       <BottomRow>
-        <TradeHistory />
+        <TradeHistory num={10} />
         <PersonalTradeHistory />
         <PersonalOpenOrders />
       </BottomRow>
