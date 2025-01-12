a625a77310ebedaa837513e49c5ce31b56896bd3
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-12 19:21:08 +0100

    Update PersonalOpenOrders.js

--- src/components/PersonalOpenOrders.js
+++ src/components/PersonalOpenOrders.js
@@ -1,15 +1,15 @@
 import { useSelector } from 'react-redux';
 import { FieldSet } from 'nexus-module';
-import { OrderTable } from './styles';
+import { MyOrdersTable, MyOrdersTableRow } from './styles';
 import DeleteButton from './DeleteButton';
 
 export default function PersonalOpenOrders() {
   const baseToken = useSelector((state) => state.ui.market.marketPairs.baseToken);
   const quoteToken = useSelector((state) => state.ui.market.marketPairs.quoteToken);
-  const myOrders = useSelector((state) => state.ui.market.myOrders);
+  const myOrders = useSelector((state) => state.ui.market.myOrders.orders);
 
   // If no orders, display “No orders” row
-  if (!myOrders || (myOrders.bids?.length === 0 && myOrders.asks?.length === 0)) {
+  if (!myOrders || myOrders?.length === 0 ) {
     return (
       <div>
         <FieldSet legend="My Open Orders">
@@ -25,41 +25,47 @@ export default function PersonalOpenOrders() {
     );
   } else {
 
+    /*
+    const myAsks = myOrders.filter((order) => order.type === 'ask');
     // Adjust 'asks' so that order.amount matches contract.amount
-    if (myOrders.asks.length > 0) {
-      myOrders.asks.forEach((element) => {
-        element.order.amount = element.contract.amount;
+    if (myAsks.length > 0) {
+      myOrders.forEach((element) => {
+        if (myOrders.type === 'ask') {
+          element.order.amount = element.contract.amount;
+        }
       });
     };
-
+    */
     // Merge and sort
-    const sortedOrders = [...myOrders.bids, ...myOrders.asks].sort(
+    const sortedOrders = myOrders.sort(
       (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
     );
 
     // Convert each order into a table row
     const rows = sortedOrders.map((order, index) => (
-      <tr key={index}>
+      <MyOrdersTableRow key={index} orderType={order.type}>
         <td>{`${order.price} ${quoteToken}`}</td>
-        <td>{`${order.order.amount} ${baseToken}`}</td>
+        <td>{`${order.contract.amount} ${order.contract.ticker}`}</td>
+        <td>{`${order.order.amount} ${order.order.ticker}`}</td>
         <td>{new Date(order.timestamp).toLocaleString()}</td>
-        <DeleteButton order={order.address} />
-      </tr>
+        <DeleteButton order={order.txid} />
+      </MyOrdersTableRow>
     ));
 
     return (
       <div>
         <FieldSet legend="My Open Orders">
-          <OrderTable>
+          <MyOrdersTable>
             <thead>
               <tr>
-                <th>Price [{quoteToken}/{baseToken}]</th>
-                <th>Amount {baseToken}</th>
+                <th>Price</th>
+                <th>Amount</th>
+                <th>Amount</th>
                 <th>Time</th>
               </tr>
             </thead>
             <tbody>{rows}</tbody>
-          </OrderTable>
+          </MyOrdersTable>
         </FieldSet>
       </div>
     );
