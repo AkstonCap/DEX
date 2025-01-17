8519fa585a5e474435d643906c0ae141ed2a51d7
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-17 22:23:42 +0100

    Update myTrades.js

--- src/reducers/ui/market/myTrades.js
+++ src/reducers/ui/market/myTrades.js
@@ -1,16 +1,15 @@
 import * as TYPE from 'actions/types';
 
 const initialState = {
-        bids: [],
-        asks: [],
+        executed: [],
+
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case TYPE.SET_MY_TRADES:
             return {
-                    bids: action.payload.bids || [],
-                    asks: action.payload.asks || [],
+                    executed: action.payload.executed || [],
             };
             
         default:
