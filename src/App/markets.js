be27dea13982c13580cf068e1b8584d28fd296fd
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-18 18:02:55 +0100

    Create markets.js

---
+++ src/App/markets.js
@@ -0,0 +1,13 @@
+import { ChartPageLayout } from "components/styles";
+
+export default function Markets() {
+  return (
+    <ChartPageLayout>
+        <div>
+            <p>
+                List of trending market pairs coming soon...
+            </p>
+        </div>
+    </ChartPageLayout>
+  );
+}
