8910f965ba1a07b2d09098482453041b8756d4b3
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-18 23:58:26 +0100

    Create BidBook.js

---
+++ src/components/BidBook.js
@@ -0,0 +1,54 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { FieldSet } from 'nexus-module';
+import { setOrder } from 'actions/actionCreators';
+import { OrderTable, OrderbookTableHeader, OrderbookTableRow } from './styles';
+
+export default function BidBook({ num }) {
+  const dispatch = useDispatch();
+  const orderBook = useSelector((state) => state.ui.market.orderBook);
+  const quoteToken = useSelector((state) => state.ui.market.marketPairs.quoteToken);
+  const baseToken = useSelector((state) => state.ui.market.marketPairs.baseToken);
+  const baseTokenDecimals = useSelector((state) => state.ui.market.marketPairs.baseTokenDecimals);
+  const quoteTokenDecimals = useSelector((state) => state.ui.market.marketPairs.quoteTokenDecimals);
+
+  const handleOrderClick = (order) => {
+    dispatch(setOrder(order.txid, order.price, order.contract.amount, order.type, order.market, 'execute'));
+  };
+
+  const renderBids = (data) => {
+    if (!Array.isArray(data)) {
+      return null;
+    }
+    const len = data.length;
+    return data.slice(0, Math.min(num, len)).map((item, index) => (
+      <OrderbookTableRow
+      key={index}
+      onClick={() => handleOrderClick(item)}
+      orderType={item.type}
+      >
+      <td>{parseFloat(item.price).toFixed(Math.min(3, quoteTokenDecimals))}</td>
+      <td>{`${parseFloat(item.order.amount).toFixed(Math.min(3, baseTokenDecimals))} ${item.order.ticker}`}</td>
+      <td>{`${parseFloat(item.contract.amount).toFixed(Math.min(3, quoteTokenDecimals))} ${item.contract.ticker}`}</td>
+      </OrderbookTableRow>
+    ));
+  };
+
+  return (
+    <div>
+      <FieldSet legend="Bids">
+          <div>
+            <OrderTable>
+              <OrderbookTableHeader>
+                <tr>
+                  <th>Price</th>
+                  <th>Amount [{baseToken}]</th>
+                  <th>Amount [{quoteToken}]</th>
+                </tr>
+              </OrderbookTableHeader>
+              <tbody>{renderBids(orderBook.bids)}</tbody>
+            </OrderTable>
+          </div>
+        </FieldSet>
+    </div>
+  );
+}
