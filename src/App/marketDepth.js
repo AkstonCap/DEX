733030ed0ef2fd9fb7e1de6463c8d49e3d2a2fab
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-01-18 23:58:14 +0100

    Update marketDepth.js

--- src/App/marketDepth.js
+++ src/App/marketDepth.js
@@ -1,15 +1,20 @@
-import { ChartPageLayout } from "components/styles";
+import { DepthPageLayout, DepthPageBottomRow } from "components/styles";
 import OrderBookComp from "components/OrderBookComp";
+import BidBook from "components/BidBook";
+import AskBook from "components/AskBook";
 
 export default function MarketDepth() {
   return (
-    <ChartPageLayout>
-      <div>
-          <p>
-            Market Depth coming soon..
-          </p>
-        </div>
-      <OrderBookComp num={20} />
-    </ChartPageLayout>
+    <DepthPageLayout>
+      <div className="text-center">
+        <p>
+          Market depth chart coming soon..
+        </p>
+      </div>
+      <DepthPageBottomRow>
+        <BidBook num={50} />
+        <AskBook num={50} />
+      </DepthPageBottomRow>
+    </DepthPageLayout>
   );
 }
