988dd3402406052b3d8c7a7d603d691cdee011ad
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-02-08 20:46:17 +0100

    Update styles.js

--- src/components/styles.js
+++ src/components/styles.js
@@ -20,6 +20,23 @@ export const PageLayout = styled.div`
 `;
 /*height: 100vh;*/
 
+export const DualColRow = styled.div`
+  display: grid;
+  grid-template-columns: 49% 49%;
+  gap: 10px;
+  overflow: auto;
+`;
+
+export const SingleColRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  gap: 10px;
+  overflow: auto;
+  width: 98%;
+  margin: 0 auto;
+`;
+
 export const TopRow = styled.div`
   display: grid;
   grid-template-columns: 59% 40%;
@@ -334,3 +351,49 @@ export const TickerText = styled.span`
   border-radius: 3px;
 `;
 //*/
+
+export const MarketsTableHeader = styled.thead`
+  color: white;
+  th {
+    padding-right: 12px;
+  }
+`;
+
+export const WideMarketsTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 8px;
+
+  th:nth-of-type(1) {
+    width: 24%;
+  }
+
+  th:nth-of-type(2) {
+    width: 24%;
+  }
+
+  th:nth-of-type(3) {
+    width: 24%;
+  }
+
+  th:nth-of-type(4) {
+    width: 24%;
+  }
+
+  th {
+    padding: 2px;
+    text-align: right;
+    padding-right: 5px;
+    font-size: 18px;
+    color: #6dcefe;
+  }
+
+  td {
+    padding: 2px;
+    text-align: right;
+    padding-right: 8px;
+    font-size: 12px;
+    color: white;
+  }
+
+`;
