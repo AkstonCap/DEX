5a58fb74a4e2f7659f58eeaddaec265bae9c3952
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-05-16 16:02:47 +0200

    Update fetchTokenAttributes.js

--- src/actions/fetchTokenAttributes.js
+++ src/actions/fetchTokenAttributes.js
@@ -4,12 +4,12 @@ import { setMarketPair } from 'actions/actionCreators';
 export const refreshMarket = (baseToken, quoteToken) => async dispatch => {
     try {
       const baseTokenData = baseToken !== 'NXS' 
-        ? await apiCall('register/get/finance:token/decimals,currentsupply,maxsupply', { name: baseToken })
-        : { decimals: 8, currentsupply: 0, maxsupply: 0 };
+        ? await apiCall('register/get/finance:token/decimals,currentsupply,maxsupply,address', { name: baseToken })
+        : { decimals: 8, currentsupply: 0, maxsupply: 0, address: '0' };
         
       const quoteTokenData = quoteToken !== 'NXS'
-        ? await apiCall('register/get/finance:token/decimals,currentsupply,maxsupply', { name: quoteToken })
-        : { decimals: 8, currentsupply: 0, maxsupply: 0 };
+        ? await apiCall('register/get/finance:token/decimals,currentsupply,maxsupply,address', { name: quoteToken })
+        : { decimals: 8, currentsupply: 0, maxsupply: 0, address: '0' };
   
       dispatch(setMarketPair(
         baseToken,
@@ -19,7 +19,9 @@ export const refreshMarket = (baseToken, quoteToken) => async dispatch => {
         baseTokenData.currentsupply,
         quoteTokenData.currentsupply,
         baseTokenData.decimals,
-        quoteTokenData.decimals
+        quoteTokenData.decimals,
+        baseTokenData.address,
+        quoteTokenData.address,
       ));
 
     } catch (error) {
