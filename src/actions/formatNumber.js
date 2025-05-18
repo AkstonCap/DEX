c4567ab6b31f67e859405b483005c1fedc4e8e3d
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-05-18 17:43:14 +0200

    Update formatNumber.js

--- src/actions/formatNumber.js
+++ src/actions/formatNumber.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function formatNumberWithLeadingZeros(number, decimals) {
+export function formatNumberWithLeadingZeros(number, decimals, tokenDecimals=6) {
   // compute the smallest value that still prints as “0.00…1”
   const threshold = 1 / Math.pow(10, decimals);
 
@@ -10,9 +10,12 @@ export function formatNumberWithLeadingZeros(number, decimals) {
     const exponent0 = parseInt(exp0, 10);              // e.g. "-4"
     const leadingZeros0 = Math.abs(exponent0) - 1;      // how many zeros before the first sig‐digit
     return number.toFixed(Math.max(decimals+leadingZeros0, 2));
-  } else if (Math.abs(number) >= 1) {
+  } else if (Math.abs(number) >= 1 && tokenDecimals > 1) {
     // ≥ threshold → normal fixed
     return number.toFixed(2);
+  } else if (Math.abs(number) >= 1 && tokenDecimals >= 0 && tokenDecimals <= 1) {
+    // ≥ threshold → normal fixed
+    return number.toFixed(tokenDecimals);
   } else if ( number === 0 ) {
     // exactly zero → normal fixed
     return number.toFixed(0);
