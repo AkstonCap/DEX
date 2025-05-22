3504f97fe6377a2076ff4515c37dd1a661a5184e
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-05-22 12:42:41 +0200

    Update RefreshButton.js
    Allow tokens without global name

--- src/App/RefreshButton.js
+++ src/App/RefreshButton.js
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { Icon, Tooltip, Button, apiCall, showErrorDialog } from 'nexus-module';
 import { setMarketPair } from 'actions/actionCreators';
 import { fetchMarketData } from 'actions/fetchMarketData';
+//import market from 'reducers/ui/market';
 
 const spin = keyframes`
   from {
@@ -28,9 +29,89 @@ function useRefreshMarket(baseTokenField, quoteTokenField) {
       let quoteTokenAttributes;
       let baseTokenExist;
       let quoteTokenExist;
-      
-      // Check token attributes if not NXS, and if existing with global name
+      let baseTokenGlobal;
+      let quoteTokenGlobal;
+      let baseTokenNonGlobal;
+      let quoteTokenNonGlobal;
+
+      // Check if inserted baseTokenField and quoteTokenField are global names or token addresses
       if ( baseTokenField !== 'NXS' ) {
+        
+        const check = await apiCall('register/get/names:global',
+          { name: baseTokenField }
+        ).catch((error) => {
+          baseTokenGlobal = false;
+        });
+
+        if (check.address) {
+          baseTokenGlobal = true;
+          baseTokenExist = true;
+        } else {
+          baseTokenGlobal = false;
+        };
+
+      } else {
+        baseTokenGlobal = true;
+        baseTokenExist = true;
+      };
+
+      if ( baseTokenGlobal === false ) {
+        const check = await apiCall('register/get/finance:token',
+          { address: baseTokenField }
+        ).catch((error) => {
+          baseTokenNonGlobal = false;
+          baseTokenExist = false;
+        });
+
+        if (check.address) {
+          baseTokenNonGlobal = true;
+          baseTokenExist = true;
+        } else {
+          baseTokenNonGlobal = false;
+          baseTokenExist = false;
+        };
+        
+      };
+
+      if ( quoteTokenField !== 'NXS' ) {
+        
+        const check = await apiCall('register/get/names:global',
+          { name: quoteTokenField }
+        ).catch((error) => {
+          quoteTokenGlobal = false;
+        });
+
+        if (check.address) {
+          quoteTokenGlobal = true;
+          quoteTokenExist = true;
+        } else {
+          quoteTokenGlobal = false;
+        };
+
+      } else {
+        quoteTokenGlobal = true;
+      };
+
+      if ( quoteTokenGlobal === false ) {
+        const check = await apiCall('register/get/finance:token',
+          { address: quoteTokenField }
+        ).catch((error) => {
+          quoteTokenNonGlobal = false;
+          quoteTokenExist = false;
+        });
+
+        if (check.address) {
+          quoteTokenNonGlobal = true;
+          quoteTokenExist = true;
+        } else {
+          quoteTokenNonGlobal = false;
+          quoteTokenExist = false;
+        };
+        
+      };
+
+      // Update token attributes if the token exists, and if existing with global name
+      if ( baseTokenField !== 'NXS' && baseTokenGlobal === true ) {
         
         baseTokenAttributes = await apiCall(
           'register/get/finance:token/decimals,currentsupply,maxsupply,address', 
@@ -43,23 +124,39 @@ function useRefreshMarket(baseTokenField, quoteTokenField) {
           }));
         });
 
+        /*
         if (baseTokenExist !== false) {
           baseTokenExist = true;
-        } 
+        }
+        */
+      } else if (baseTokenField !== 'NXS' && baseTokenNonGlobal === true) {
+
+        baseTokenAttributes = await apiCall(
+          'register/get/finance:token/decimals,currentsupply,maxsupply,address', 
+          { address: baseTokenField }
+        ).catch((error) => {
+          baseTokenExist = false;
+          dispatch(showErrorDialog({
+            message: 'Cannot get base token attributes from apiCall',
+            note: error?.message || 'Unknown error',
+          }));
+        });
 
       } else if (baseTokenField === 'NXS') {
-        baseTokenExist = true;
+        //baseTokenExist = true;
         baseTokenAttributes = {
           decimals: 8,
           currentsupply: 0,
           maxsupply: 0,
           address: '0'
         }
+
       } else {
         baseTokenExist = false;
       }
 
-      if ( quoteTokenField !== 'NXS' ) {
+      if ( quoteTokenField !== 'NXS' && quoteTokenGlobal === true ) {
+        
         quoteTokenAttributes = await apiCall(
           'register/get/finance:token/decimals,currentsupply,maxsupply,address', 
           { name: quoteTokenField }
@@ -71,19 +168,35 @@ function useRefreshMarket(baseTokenField, quoteTokenField) {
           }));
           }
         );
-
+        /*
         if (quoteTokenExist !== false) {
           quoteTokenExist = true;
         }
+        */
+
+      } else if (quoteTokenField !== 'NXS' && quoteTokenNonGlobal === true) {
+        
+        quoteTokenAttributes = await apiCall(
+          'register/get/finance:token/decimals,currentsupply,maxsupply,address', 
+          { address: quoteTokenField }
+        ).catch((error) => {
+          quoteTokenExist = false;
+          dispatch(showErrorDialog({
+            message: 'Cannot get quote token attributes from apiCall',
+            note: error?.message || 'Unknown error',
+          }));
+        });
 
       } else if (quoteTokenField === 'NXS') {
-        quoteTokenExist = true;
+        
+        //quoteTokenExist = true;
         quoteTokenAttributes = {
           decimals: 8,
           currentsupply: 0,
           maxsupply: 0,
           address: '0'
         }
+
       } else {
         quoteTokenExist = false;
       }
@@ -91,7 +204,10 @@ function useRefreshMarket(baseTokenField, quoteTokenField) {
       // Set the market pair if tokens exists
       if (baseTokenExist === true && quoteTokenExist === true) {
 
+        const marketPair = `${baseTokenField}/${quoteTokenField}`;
+
         dispatch(setMarketPair(
+          marketPair,
           baseTokenField, 
           quoteTokenField, 
           baseTokenAttributes.maxsupply, 
