ed993e7d5a554cc71b47394245be2211a3929d1b
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2025-05-24 22:50:08 +0200

    Update BidBook.js

--- src/components/BidBook.js
+++ src/components/BidBook.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { FieldSet } from 'nexus-module';
 import { setOrder } from 'actions/actionCreators';
-import { OrderTable, OrderbookTableHeader, OrderbookTableRow } from './styles';
+import { OrderTable, OrderbookTableHeader, OrderbookTableRow, formatTokenName } from './styles';
 import { formatNumberWithLeadingZeros } from '../actions/formatNumber';
 
 export default function BidBook({ num }) {
@@ -63,9 +63,9 @@ export default function BidBook({ num }) {
             <OrderTable>
               <OrderbookTableHeader>
                 <tr>
-                  <th>Price</th>
-                  <th>Amount [{baseToken}]</th>
-                  <th>Amount [{quoteToken}]</th>
+                  <th>Price [{formatTokenName(quoteToken)}]</th>
+                  <th>Amount [{formatTokenName(baseToken)}]</th>
+                  <th>Amount [{formatTokenName(quoteToken)}]</th>
                 </tr>
               </OrderbookTableHeader>
               <tbody>{renderBids(orderBook.bids)}</tbody>
